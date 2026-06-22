7feb01f954b718ec6af1c260fcae47985c739e4c
eijeiejie-sketch <eijeiejie@gmail.com>
2026-06-22 21:28:01 +0700

    Update segitiga.js

--- Alwi sihab/segitiga.js
+++ Alwi sihab/segitiga.js
@@ -3,7 +3,7 @@ let alas = Number(prompt('masukan alas : '))
 let tinggi = Number(prompt('masukan tinggi : '))
 
 let luas = 0.5 * alas * tinggi
-let keliling = alas + tinggi + (Math.sqrt   (alas^2 + tinggi^2))
+let keliling = alas + tinggi + alas ^ 2 + tinggi ^ 2
 
 console.log('===============')
 console.log('   H A S I L   ')
