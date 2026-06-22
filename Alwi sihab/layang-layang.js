6ac3f4c19a58b3e88409f7d391ce56dfd5afcd9f
eijeiejie-sketch <eijeiejie@gmail.com>
2026-06-22 21:27:06 +0700

    Update layang-layang.js

--- Alwi sihab/layang-layang.js
+++ Alwi sihab/layang-layang.js
@@ -3,7 +3,7 @@ let diagonal1 = Number(prompt('Masukan diagonal : '))
 let diagonal2 = Number(prompt('Masukan diagonal : '))
 
 let luas = 0.5 * diagonal1 * diagonal2
-let keliling = diagonal1 + diagonal2 + (Math.sqrt(Math.pow(diagonal1, 2) + Math.pow(diagonal2, 2)))
+let keliling = diagonal1 + diagonal2 + (diagonal1 ^ 2) + (diagonal2 ^ 2)
 
 console.log('============')
 console.log('  H A S I L ')
